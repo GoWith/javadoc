f99742686b0b028604999e084bd113358959e92f
maoyi <maoyi@jd.com>
2018-12-04 16:44:13 +0800

    javadoc demo

---
+++ src/main/java/com/jd/crm/api/web/SpringBootStartApplication.java
@@ -0,0 +1,19 @@
+package com.jd.crm.api.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Create by maoyi on 2018/12/4
+ * don't worry be happy!
+ *
+ * @author maoyi
+ */
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(ApiWebApplication.class);
+    }
+
+}
